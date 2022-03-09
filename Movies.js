dd377ee9863d86f7bfbbb225dac12473aefac1b1
tanner <tanner@wiechdoctor.net>
2022-03-09 12:56:59 -0700

    added connect code

--- Movies.js
+++ Movies.js
@@ -3,6 +3,16 @@ var Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
 
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
+
+
+
 var MovieSchema = new Schema({
     title: {type: String, required: true},
     year: {type: Number, required: true},
